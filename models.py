9a45c341ea5ef135fcacfb6eae72c5895e5e2ef1
Sonali Waghmare <Sonali@fixstars.com>
2019-07-07 19:35:02 -0700

    Basic module

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 ## TODO: define the convolutional neural network architecture
 
 import torch
-from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 # can use the below import should you choose to initialize the weights of your Net
@@ -20,18 +19,44 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
+        ## output size = (W-F)/S +1 = (224-5)/1 +1 = 220 --> (32,220,220) --> pool => (32,110,110)
+        self.conv1 = nn.Conv2d(1, 32, 5)   
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        
+        ## output size = (W-F)/S +1 = (110-5)/1 +1 = 106 --> (32,106,106) --> pool => (32,53,53)
+        self.conv2 = nn.Conv2d(32, 64, 5)
+
+        # maxpool layer
+        # pool with kernel_size=2, stride=2
+        self.pool = nn.MaxPool2d(2, 2)
+        
+        # 64*53*53 = 179776/136 --> 1321
+        self.fc1 = nn.Linear(64*53*53, 1321) 
+        
+        # dropout with p=0.4
+        self.fc1_drop = nn.Dropout(p=0.4)
+        
+        #final output
+        self.fc2 = nn.Linear(1321, 136)
         
 
         
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
-        ## x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+
+        # prep for linear layer
+        # flatten the inputs into a vector
+        x = x.view(x.size(0), -1)
         
+        # two linear layers with dropout in between
+        x = F.relu(self.fc1(x))
+        x = self.fc1_drop(x)
+        x = self.fc2(x)
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
